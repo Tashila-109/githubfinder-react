c7e1ac23f7bf9b972def0ca2d835d922f8c385b0
Tashila Fernando <tashila97@hotmail.com>
2019-11-14 23:11:08 +0530

    Init commit

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1>Page Not Found</h1>
+            <p className="lead">The page with that URL does not exist.</p>
+        </div>
+    )
+}
+
+export default NotFound
